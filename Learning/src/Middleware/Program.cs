fae125a7db9db1459571a618e45286c69233b2df
jonathan.busuttil <jonathan.busuttil@gig.com>
2023-04-19 09:35:56 +0200

    added error handler

--- Learning/src/Middleware/Program.cs
+++ Learning/src/Middleware/Program.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapPost("/login", (LoginRequest loginRequest) => $"User: [{loginRequest.Username}] Logged In Successfully");
 
-app.UseMiddleware<RequestResponseLoggingMiddleware>()
-   .UseMiddleware<ValidationMiddleware>();
+app.UseMiddleware<ErrorHandlingMiddleware>() //here we are instructing the app to use these middlewares on every request
+   .UseMiddleware<RequestResponseLoggingMiddleware>(); 
+
+//here we are instructing the app to use this middleware only on the login route
+app.UseWhen(context => context.Request.Path.StartsWithSegments("/login"), appBuilder =>
+{
+    appBuilder.UseMiddleware<ValidationMiddleware>();
+});
 
 app.Run();
 
@@ -26,11 +35,13 @@ public class RequestResponseLoggingMiddleware
 
     public async Task InvokeAsync(HttpContext context)
     {
-        Console.WriteLine("Login Attempt");
+        string path = context.Request.Path;
+        
+        Console.WriteLine($"{path} Attempt");
         
         await _next(context); //this will call the next middleware in the chain if there is one
         
-        Console.WriteLine("Login Success");
+        Console.WriteLine($"{path} Success");
     }
 }
 
@@ -54,9 +65,40 @@ public class ValidationMiddleware
         LoginRequest request = System.Text.Json.JsonSerializer.Deserialize<LoginRequest>(bodyAsText);
 
         if (string.IsNullOrEmpty(request.Username) || string.IsNullOrEmpty(request.Password))
-            throw new Exception("Invalid Login Request");
+            throw new ValidationException("Invalid Login Request");
 
         await _next(context);
+    }
+}
 
+public class ErrorHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ErrorHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            //the only job of this middleware is to be a catch all and not let any exception details returned to the caller.
+            await _next(context);
+        }
+        catch (ValidationException e)
+        {
+            Console.WriteLine(e);
+
+            //instead of letting the exception bubble up to the caller, we are writing a server log and returning the correct http response code
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        }
     }
 }
